201e5038d9c61a0ced308a0926ec25124d6d06c9
Jonathan <jonathantyt@u.nus.edu>
2023-03-19 22:39:17 +0800

    Add long document splitting procedure

--- augment.py
+++ augment.py
@@ -1,10 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Augmentation script using NLPAUG to generate more training samples
-for regularising the transformer finetuning process.
-"""
-
 import os
+import nltk
 import argparse
 import numpy as np
 import pandas as pd
@@ -25,6 +20,8 @@ root = '/home/svu/e0425991/bert/'
 df_train = pd.read_csv(os.path.join(root, './data/fulltrain.csv'), header=None)
 df_train.columns = ['cls', 'text']
 
+df_train = df_train.drop_duplicates()
+
 df_train['num_tokens'] = df_train['text'].apply(lambda x: len(wordpunct_tokenize(x)))
 df_train = df_train[df_train['num_tokens'] >= 10].reset_index(drop=True)
 df_train = df_train.drop(columns='num_tokens')
@@ -40,8 +37,35 @@ df_val = df_train.loc[val_idx]
 df_val.to_csv('./data/validation.csv', index=False, header=False)
 print('Num Validation Samples:', df_val.shape[0])
 
+# ----- Split Long Documents for More Samples
+
 df_train = df_train.loc[train_idx].reset_index(drop=True)
-df_train.to_csv('./data/train.csv')
+
+def split_document(text):
+    
+    split_text, curr_text = [], []
+    max_len, curr_len = 512, 0
+    
+    for sentence in nltk.sent_tokenize(text):
+        tokens = nltk.wordpunct_tokenize(sentence)
+        tokens_len = len(tokens)
+        
+        if curr_len < max_len:
+            curr_text.extend(tokens)
+            curr_len += tokens_len
+        else:
+            split_text.append(' '.join(curr_text))
+            curr_text, curr_len = tokens, tokens_len
+            
+    if curr_len >= 10:
+        split_text.append(' '.join(curr_text))
+    
+    return split_text
+
+df_train['text'] = df_train['text'].apply(split_document)
+df_train = df_train.explode('text')
+
+df_train.to_csv('./data/train_split.csv')
 
 # ----- Apply Augmentations to Training Data
 
@@ -65,7 +89,8 @@ problem_idxs = []
 for idx, (text, label) in tqdm(enumerate(zip(texts, labels))):
     try:
         aug_text = aug.augment(text, n=3)
-        aug_data.append((label, aug_text))
+        for aug_sample in aug_text:
+            aug_data.append((label, aug_sample))
     except:
         print(text, cls_names[label-1], idx)
 
